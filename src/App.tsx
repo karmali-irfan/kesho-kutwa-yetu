e45b9109d8d880b9d0284bee4073d92b1e0569e6
karmali-irfan <ikarmali79@gmail.com>
2025-11-11 20:47:15 +0100

    changes to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/kesho-kutwa-yetu">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
