ae547e8952b0e05df59f6ccf75c813486bdf7747
karmali-irfan <ikarmali79@gmail.com>
2025-11-11 19:40:38 +0100

    new build with vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// Replace this with your GitHub repo name
+const repoName = "keshokutwa-yetu";
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: `/${repoName}/`, // 👈 This fixes the blank page issue
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
